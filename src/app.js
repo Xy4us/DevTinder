6faa30964281a30b354afa9aecd81c063146d897
Xy4us <sanbhutnik@gmail.com>
2026-03-21 17:44:01 +0530

    hi

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ const express = require("express");
 
 const app = express(); // creating an express application
 
-app.use("/hello", (req, res) => {
-  res.send("Hello, World!");
+//This will match only GET HTTP method API calls to /user endpoint
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "John",
+    lastName: "Doe",
+  });
 });
 
-app.use("/hi", (req, res) => {
-  res.send("Hi, World!");
+app.post("/user", (req, res) => {
+  // Saving Data to DB
+  res.send("Data Saved Successfully!");
 });
 
+app.patch("/user", (req, res) => {
+  // Updating Data in DB
+  res.send("Data Updated Successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  // Deleting Data from DB
+  res.send("Data Deleted Successfully!");
+});
+
+//This will match all the HTTP methods API calls to test (eg. GET, POST, etc.)
 app.use("/test", (req, res) => {
   res.send("Hello from server!");
 });
