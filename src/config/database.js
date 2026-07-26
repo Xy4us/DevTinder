5cbc72352f551ea21e446e315f7c83466e3ff481
Ayush Kumar <sanbhutnik@gmail.com>
2026-07-26 16:07:55 +0530

    made some changees

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Set public DNS fallback (Google/Cloudflare) to resolve MongoDB SRV records on systems where ISP/router DNS fails
+try {
+  dns.setServers(["8.8.8.8", "1.1.1.1"]);
+} catch (e) {
+  // Ignore if setting custom DNS servers fails in restricted environment
+}
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
+
